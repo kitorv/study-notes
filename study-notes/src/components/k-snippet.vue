cdba7c06f6abc2435e48578d58824414fc3300d9
kitorv <826526881@qq.com>
2018-11-27 16:34:36 +0800

    标题用h标签设置

--- study-notes/src/components/k-snippet.vue
+++ study-notes/src/components/k-snippet.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="k-snippet">
-    <div class="k-snippet--title">
+    <h2 class="k-snippet--title">
       <slot name="title"></slot>
-    </div>
+    </h2>
     <slot></slot>
   </div>
 </template>
