b12264ca9ae89a2de12ef10dbc5fbe8c9a05b452
kitorv <826526881@qq.com>
2018-12-04 17:01:54 +0800

    预渲染配置

--- src/main.js
+++ src/main.js
@@ -8,5 +8,8 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
-  render: h => h(App)
+  render: h => h(App),
+  mounted() {
+    document.dispatchEvent(new Event("render-event"))
+  }
 }).$mount("#app")
--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,10 @@ const kMarkdownSnippetHtml = require("./webpack/k-markdown-snippet-html.js")
 const kMarkdownSnippetCss = require("./webpack/k-markdown-snippet-css.js")
 const kMarkdownSnippetJavascript = require("./webpack/k-markdown-snippet-javascript.js")
 
+const PrerenderSPAPlugin = require("prerender-spa-plugin")
+const Renderer = PrerenderSPAPlugin.PuppeteerRenderer
+const path = require("path")
+
 module.exports = {
   // 配置【vue-markdown-loader】解析md格式的文件
   chainWebpack: config => {
@@ -25,5 +29,31 @@ module.exports = {
         },
         use: [kMarkdownSnippet, kMarkdownSnippetHtml, kMarkdownSnippetCss, kMarkdownSnippetJavascript]
       })
+  },
+  configureWebpack: config => {
+    if (process.env.NODE_ENV !== "production") return
+    return {
+      plugins: [
+        new PrerenderSPAPlugin({
+          // 生成文件的路径，也可以与webpakc打包的一致。
+          // 下面这句话非常重要！！！
+          // 这个目录只能有一级，如果目录层次大于一级，在生成的时候不会有任何错误提示，在预渲染的时候只会卡着不动。
+          staticDir: path.join(__dirname, "dist"),
+
+          // 对应自己的路由文件，比如a有参数，就需要写成 /a/param1。
+          routes: ["/", "/home/plan"],
+
+          // 这个很重要，如果没有配置这段，也不会进行预编译
+          renderer: new Renderer({
+            inject: {
+              foo: "bar"
+            },
+            headless: false,
+            // 在 main.js 中 document.dispatchEvent(new Event('render-event'))，两者的事件名称要对应上。
+            renderAfterDocumentEvent: "render-event"
+          })
+        })
+      ]
+    }
   }
 }
